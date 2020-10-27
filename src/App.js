852bc8f29935379550bece443b4211965a1936cf
Jaren Smart <jarensmart@yahoo.com>
2020-10-26 22:45:57 -0400

    Added wordsList & setup state needed later

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Hangman from "./components/Hangman";
@@ -6,8 +6,15 @@ import WrongLetters from "./components/WrongLetters";
 import Word from "./components/Word";
 import MessageModal from "./components/MessageModal";
 import Notification from "./components/Notification";
+import wordsList from "./components/WordsList";
+
+let selectedWord = wordsList[Math.floor(Math.random() * wordsList.length)];
 
 function App() {
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+
   return (
     <div>
       <Header />
