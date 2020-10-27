41a9d77941fbba427248f80d22432bfaad340ffa
Jaren Smart <jarensmart@yahoo.com>
2020-10-26 22:10:59 -0400

    added components & cleaned up unused files

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function header() {
+  return (
+    <>
+      <h1>Hangman</h1>
+      <p>Find the hidden word - Enter a letter</p>
+    </>
+  );
+}
+
+export default header;
